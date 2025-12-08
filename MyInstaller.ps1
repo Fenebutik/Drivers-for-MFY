69e22dd2cc7e5ab986dc1bd17a92697b09d874c7
Fenebutik <uzumetor@gmail.com>
2025-12-08 12:46:19 +0300

    Update MyInstaller.ps1

--- MyInstaller.ps1
+++ MyInstaller.ps1
@@ -29,7 +29,7 @@ while ($true) {
             # --- КОНФИГУРИРУЕМАЯ ЧАСТЬ: Настройки для Kyocera ---
             $DriverName = "Kyocera TWAIN Driver"
             # ССЫЛКА 1: Укажи прямую ссылку на скачивание .exe файла
-            $DownloadUrl = "https://github.com/Fenebutik/Drivers-for-MFY/raw/refs/heads/main/KyoceraTWAINDriver2.1.2822_1.4rc9.exe"
+            $DownloadUrl = "https://raw.githubusercontent.com/Fenebutik/Drivers-for-MFY/main/KyoceraTWAINDriver_2.1.2822_1.4rc9.exe"
             # ИМЯ ФАЙЛА: Как он сохранится локально (можно оставить как в ссылке)
             $LocalFileName = "Kyocera_TWAIN_Driver_2.1.2822_1.4rc9.exe"
             # ПАПКА: Где сохранить (временная папка текущего пользователя)
@@ -67,6 +67,7 @@ while ($true) {
         }
     }
 }
+
 
 
 
